c1ffb1fdf5961ec95aacd0b59925821c2135fb14
Shunichi Yamamoto <info@tkrworks.net>
2012-09-14 12:33:47 +0900

    Appended the license statement.

--- PIC32UBL_Max/src/hid.java
+++ PIC32UBL_Max/src/hid.java
@@ -1,3 +1,24 @@
+/*
+ * Copylight (C) 2012, Shunichi Yamamoto, tkrworks.net
+ *
+ * This file is part of PIC32UBL_Max.
+ *
+ * PIC32UBL_Max is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option ) any later version.
+ *
+ * PICrouter is distributed in the hope that it will be useful,
+ * but WITHIOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.   See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PICrouter. if not, see <http:/www.gnu.org/licenses/>.
+ *
+ * hid.java,v.0.9 2012/09/14
+ */
+
 import java.io.*;
 import com.cycling74.max.*;
 
