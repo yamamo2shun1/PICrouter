0a2f922fc55b68803f03c6ef879bd2b897da504e
Shunichi Yamamoto <info@tkrworks.net>
2013-05-25 10:59:42 +0900

    Modified some HID parameters for USB host.

--- PICrouter.X/usb_config.c
+++ PICrouter.X/usb_config.c
@@ -1,39 +1,23 @@
 /*
-********************************************************************************
-                                                                                
-Software License Agreement                                                      
-                                                                                
-Copyright � 2007-2008 Microchip Technology Inc. and its licensors.  All         
-rights reserved.                                                                
-                                                                                
-Microchip licenses to you the right to: (1) install Software on a single        
-computer and use the Software with Microchip 16-bit microcontrollers and        
-16-bit digital signal controllers ("Microchip Product"); and (2) at your        
-own discretion and risk, use, modify, copy and distribute the device            
-driver files of the Software that are provided to you in Source Code;           
-provided that such Device Drivers are only used with Microchip Products         
-and that no open source or free software is incorporated into the Device        
-Drivers without Microchip's prior written consent in each instance.             
-                                                                                
-You should refer to the license agreement accompanying this Software for        
-additional information regarding your rights and obligations.                   
-                                                                                
-SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY         
-KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY              
-WARRANTY OF MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A          
-PARTICULAR PURPOSE. IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE             
-LIABLE OR OBLIGATED UNDER CONTRACT, NEGLIGENCE, STRICT LIABILITY,               
-CONTRIBUTION, BREACH OF WARRANTY, OR OTHER LEGAL EQUITABLE THEORY ANY           
-DIRECT OR INDIRECT DAMAGES OR EXPENSES INCLUDING BUT NOT LIMITED TO ANY         
-INCIDENTAL, SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES, LOST PROFITS OR         
-LOST DATA, COST OF PROCUREMENT OF SUBSTITUTE GOODS, TECHNOLOGY,                 
-SERVICES, ANY CLAIMS BY THIRD PARTIES (INCLUDING BUT NOT LIMITED TO ANY         
-DEFENSE THEREOF), OR OTHER SIMILAR COSTS.                                       
-                                                                                
-********************************************************************************
-*/
-
-// Created by the Microchip USBConfig Utility, Version 0.0.12.0, 3/28/2008, 8:58:18
+ * Copylight (C) 2012, Shunichi Yamamoto, tkrworks.net
+ *
+ * This file is part of PICrouter.
+ *
+ * PICrouter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option ) any later version.
+ *
+ * PICrouter is distributed in the hope that it will be useful,
+ * but WITHIOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.   See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PICrouter. if not, see <http:/www.gnu.org/licenses/>.
+ *
+ * usb_config.c,v.1.0.0 2013/05/25
+ */
 
 #include "GenericTypeDefs.h"
 #include "HardwareProfile.h"
@@ -72,6 +56,8 @@ USB_TPL usbTPL[] =
 {
 #if 0
     { INIT_CL_SC_P( 0x03ul, 0x01ul, 0x02ul ), 0, 0, {TPL_CLASS_DRV} }, // HID Class
+    { INIT_CL_SC_P( 0x03ul, 0x01ul, 0x01ul ), 0, 0, {TPL_CLASS_DRV} }, // HID Class
+    { INIT_CL_SC_P( 0x03ul, 0x00ul, 0x00ul ), 0, 0, {TPL_CLASS_DRV} }, // HID Class
 #endif
     { INIT_CL_SC_P( 0x01ul, 0x03ul, 0x00ul ), 0, 0, {TPL_CLASS_DRV} }  // MIDI Audio Subclass
 };
--- PICrouter.X/usb_config.h
+++ PICrouter.X/usb_config.h
@@ -1,46 +1,23 @@
-/********************************************************************
- FileName:     	usb_config.h
- Dependencies: 	Always: GenericTypeDefs.h, usb_device.h
-               	Situational: usb_function_hid.h, usb_function_cdc.h, usb_function_msd.h, etc.
- Processor:		PIC18 or PIC24 USB Microcontrollers
- Hardware:		The code is natively intended to be used on the following
- 				hardware platforms: PICDEM� FS USB Demo Board, 
- 				PIC18F87J50 FS USB Plug-In Module, or
- 				Explorer 16 + PIC24 USB PIM.  The firmware may be
- 				modified for use on other USB platforms by editing the
- 				HardwareProfile.h file.
- Complier:  	Microchip C18 (for PIC18) or C30 (for PIC24)
- Company:		Microchip Technology, Inc.
-
- Software License Agreement:
-
- The software supplied herewith by Microchip Technology Incorporated
- (the �Company�) for its PIC� Microcontroller is intended and
- supplied to you, the Company�s customer, for use solely and
- exclusively on Microchip PIC Microcontroller products. The
- software is owned by the Company and/or its supplier, and is
- protected under applicable copyright laws. All rights are reserved.
- Any use in violation of the foregoing restrictions may subject the
- user to criminal sanctions under applicable laws, as well as to
- civil liability for the breach of the terms and conditions of this
- license.
-
- THIS SOFTWARE IS PROVIDED IN AN �AS IS� CONDITION. NO WARRANTIES,
- WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT NOT LIMITED
- TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
- PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. THE COMPANY SHALL NOT,
- IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL OR
- CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
-
-********************************************************************
- File Description:
-
- Change History:
-  Rev   Date         Description
-  1.0   11/19/2004   Initial release
-  2.1   02/26/2007   Updated for simplicity and to use common
-                     coding style
- *******************************************************************/
+/*
+ * Copylight (C) 2012, Shunichi Yamamoto, tkrworks.net
+ *
+ * This file is part of PICrouter.
+ *
+ * PICrouter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option ) any later version.
+ *
+ * PICrouter is distributed in the hope that it will be useful,
+ * but WITHIOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.   See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PICrouter. if not, see <http:/www.gnu.org/licenses/>.
+ *
+ * usb_config.h,v.1.0.0 2013/05/25
+ */
 
 /*********************************************************************
  * Descriptor specific type definitions are defined in: usbd.h
@@ -163,7 +140,7 @@
 
 /* Host Configuration */
 #define USB_ENABLE_TRANSFER_EVENT
-#define NUM_TPL_ENTRIES 1// 2
+#define NUM_TPL_ENTRIES 4//1// 2
 #define USB_NUM_CONTROL_NAKS 200
 //test #define USB_NUM_CONTROL_NAKS 20
 #define USB_SUPPORT_INTERRUPT_TRANSFERS
