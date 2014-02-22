12f1194ef616fc6a1cd32bd2a1123b98cd1c2964
Shunichi Yamamoto <info@tkrworks.net>
2014-02-23 00:53:58 +0900

    Modified the following build configurations.
    1. Optimize Level
    2. Minimum Heap Size
    3. mruby Build Options(cc.defines)

--- PICrouter_795_mrb.X/for_mruby/build_config.rb
+++ PICrouter_795_mrb.X/for_mruby/build_config.rb
@@ -110,7 +110,7 @@ MRuby::CrossBuild.new("PICrouter") do |conf|
 
   PICROUTER_PATH = "C:/Users/shun/MPLABXProjects/PICrouter"
   XC32_PATH = "C:/Program Files (x86)/Microchip/xc32/v1.22"
-  MRUBY_PATH = "C:/Users/shun/MPLABXProjects/PICrouter/mruby-master"
+  MRUBY_PATH = "C:/Users/shun/MPLABXProjects/PICrouter/mruby-1.0.0"
 
   conf.cc do |cc|
     cc.command="#{XC32_PATH}/bin/xc32-gcc.exe"
@@ -119,9 +119,26 @@ MRuby::CrossBuild.new("PICrouter") do |conf|
                         "#{PICROUTER_PATH}/PICrouter_795_mrb.X/USB",
                         "#{MRUBY_PATH}/include",
                         "#{XC32_PATH}/pic32mx/include"]
-    cc.flags << "-g -ffunction-sections -fdata-sections -mips16 -mprocessor=32MX795F512H -O -Wl,--defsym=__MPLAB_BUILD=1," +
-      "-Map=\"dist/default/production/PICrouter_795_mrb.X.production.map\",--defsym=_min_heap_size=102400,--defsym=_min_stack_size=1136,--gc-sections,"
+    cc.flags << "-g -ffunction-sections -fdata-sections -mips16 -mprocessor=32MX795F512H -O1 -Wl,--defsym=__MPLAB_BUILD=1," +
+      "-Map=\"dist/default/production/PICrouter_795_mrb.X.production.map\",--defsym=_min_heap_size=102284,--defsym=_min_stack_size=1136,--gc-sections,"
     cc.compile_options = "%{flags} -o %{outfile} -c %{infile}"
+    
+    cc.defines << %w(MRB_HEAP_PAGE_SIZE=64)
+    cc.defines << %w(MRB_USE_IV_SEGLIST)
+    cc.defines << %w(MRB_IVHASH_INIT_SIZE=3)
+    cc.defines << %w(KHASH_DEFAULT_SIZE=8)
+    cc.defines << %w(MRB_STR_BUF_MIN_SIZE=20)
+    cc.defines << %w(MRB_GC_STRESS)
+    #cc.defines << %w(DISABLE_STDIO)
+
+  end
+
+  conf.cxx do |cxx|
+    cxx.command = conf.cc.command.dup
+    cxx.include_paths = conf.cc.include_paths.dup
+    cxx.flags = conf.cc.flags.dup
+    cxx.defines = conf.cc.defines.dup
+    cxx.compile_options = conf.cc.compile_options.dup
   end
 
   conf.archiver do |archiver|
@@ -134,13 +151,5 @@ MRuby::CrossBuild.new("PICrouter") do |conf|
 
   #do not build test executable
   conf.build_mrbtest_lib_only
-
-  #gems from core
-  #conf.gem :core => "mruby-print"
-  #conf.gem :core => "mruby-math"
-  #conf.gem :core => "mruby-enum-ext"
-
-  #light-weight regular expression
-  #conf.gem :github => "masamitsu-murase/mruby-hs-regexp", :branch => "master" 
 
  end
